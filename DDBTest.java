25842ef6bebd5907aa6d4655536d5c462faea39a
Qingye Jiang <qjiang@ieee.org>
2017-10-11 10:32:24 +1100

    initial push

---
+++ DDBTest.java
@@ -0,0 +1,21 @@
+/**
+ * README 
+ *
+ * In this assignment, you were given the following files
+ * (1) DDBTest.java   
+ *     - The Java source file
+ * (2) firstname.txt  
+ *     - TXT file containing some popular firstnames, one name a line, with duplicate entries.
+ * (3) lastname.txt
+ *     - TXT file containing some popular lastnames, one name a line, with duplicate entries.
+ */
+
+
+public class DDBtest
+{
+
+public static void mian(string[] args)
+{
+}
+
+}
